920be8595f5e252dcf6382c2027da7524adc619d
Snochowski Mikołaj <snochopl@gmail.com>
2020-12-09 22:51:18 +0100

    Add basic Header component

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
+
+test('renders Header component', () => {
+  render(<Header />);
+});
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const Header: React.FC<Props> = () => {
+  return (
+    <div>
+      <p>Hello from Header component</p>
+    </div>
+  );
+};
+
+export default Header;
