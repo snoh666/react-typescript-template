d7fa000f852e0b1af48d40cd84bc698dc55e2dbb
Snochowski Mikołaj <snochopl@gmail.com>
2020-12-09 22:35:23 +0100

    Apply new eslint config options

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,27 @@ module.exports = {
   ],
   parserOptions: {
     project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-props-no-spreading': 'off',
+    'no-shadow': 'off',
+    'react-redux/connect-prefer-named-arguments': 'off',
+    'react-redux/prefer-separate-component-file': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/no-unresolved': 'off',
+  },
+  env: {
+    browser: true,
+    es6: true,
   },
 };
