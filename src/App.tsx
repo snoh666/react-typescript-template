99ddf2fbbed84a3b2eb2d84641966dfccf2f59fc
Snochowski Mikołaj <snochopl@gmail.com>
2020-12-10 10:40:02 +0100

    Add ThemeProvider and GlobalStyles component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
+import theme from '@styles/theme';
+import GlobalStyles from '@styles/GlobalStyles';
 import Header from '@components/Header';
 
 const App: React.FC = () => (
-  <div className='App'>
-    <header className='App-header'>
-      <Header />
-    </header>
-  </div>
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <div className='App'>
+      <header className='App-header'>
+        <Header />
+      </header>
+    </div>
+  </ThemeProvider>
 );
 
 export default App;
